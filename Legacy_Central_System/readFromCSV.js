32ad3604b4b4661fd9e04855ed0ae042426cdcc9
moonlaughs <iza8848@gmail.com>
2020-10-08 19:26:07 +0200

    Update readFromCSV.js
    ADDED: msgpack creating files

--- Legacy_Central_System/readFromCSV.js
+++ Legacy_Central_System/readFromCSV.js
@@ -3,64 +3,89 @@ const fs = require('fs');
 
 var builder = require('xmlbuilder');
 
-const express = require('express');
-const axios = require('axios');
+const msgpack = require('msgpack-lite');
 
-var app = express();
+var request = require('request');
 
 var FirstName;
 var LastName;
 var DateOfBirth;
 var Email;
+var CprNumber;
+var Address;
+var Country;
+var Phone;
+var NemId;
 
-var array = [];//[{firstName:"m1", lastname: "row.LastName", cprnumber: "row.DateOfBirth", email: "row.Email"}]
-function Person(firstname, lastname, dateofbirth, email)
-{
+function thisPerson(firstname, lastname, dob, email, cpr, address, country, phone, nemid) {
     this.FirstName = firstname;
     this.LastName = lastname;
-    this.DateOfBirth = dateofbirth;
+    this.DateOfBirth = dob;
     this.Email = email;
+    this.CprNumber = cpr;
+    this.Address = address;
+    this.Country = country;
+    this.Phone = phone;
+    this.NemId = nemid;
 }
-var i = 0;
+
 fs.createReadStream('../Main_System/people.csv')
     .pipe(csv())
     .on('data', (row) => {
-        //console.log(row.FirstName);
-        array.push(new Person(row.FirstName, row.LastName, row.DateOfBirth, row.Email));
-        console.log(array[i].FirstName)
-        generateXml(array[i]);
-        i++;
+        console.log("Reading data from csv file...")
+        generateXml(new thisPerson(row.FirstName, row.LastName, row.DateOfBirth, row.Email, generateCpr(row.DateOfBirth), row.Phone, row.Addrss, row.Country))
     })
     .on('end', () => {
-        console.log('CSV file successfully processed');
+        console.log('Process successfully processed');
     });
 
-function generateXml(data) {
-    console.log("My" + data.FirstName)
-    var root = builder.create('Person')
-    var item = root.ele('FirstName', {}, data.FirstName);
-    var item2 = root.ele('LastName', {}, data.LastName);
-    var item3 = root.ele('CprNumber', {}, generateCpr(data.DateOfBirth));
-    var item4 = root.ele('Email', {}, data.Email);
-    var xml = root.end({ pretty: true });
-    console.log(xml);
-    sendPost(xml);
+function generateXml(personData) {
+    console.log("Generating XML...")
+    var person = builder.create('Person')
+    var firstname = person.ele('FirstName', {}, personData.FirstName);
+    var lastname = person.ele('LastName', {}, personData.LastName);
+    var cprnumber = person.ele('CprNumber', {}, personData.CprNumber);
+    var email = person.ele('Email', {}, personData.Email);
+    var xml = person.end({ pretty: true });
+
+    sendPost(xml, personData);
 }
 
 function generateCpr(dateOfBirth) {
+    console.log("Generating cpr...")
     var dob = dateOfBirth.split("-");
     var cpr = dob[0] + dob[1] + dob[2] + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
     return cpr.toString();
 }
 
-function sendPost(body) {
-    
-    axios.post('http://localhost:8080/nemID', body).then(response =>{
-        console.log(response)
-        return res.status(200).send({generatedCode: response.data.generatedCode});
-    }).catch(err =>{
-        if(err){
-            console.log(err);
-        }
-    });
+function sendPost(body, personData) {
+    console.log("preparing for sending request...")
+
+    request.post({
+        headers: { 'content-type': 'text/xml' }
+        , url: 'http://localhost:8080/nemID', body: body
+    }
+        , function (error, response, body) {
+            console.log(body) //not fully working
+
+            var jsonObject = {
+                "f_name": personData.FirstName,
+                "l_name": personData.LastName,
+                "birth_date": personData.DateOfBirth,
+                "email": personData.Email,
+                "country": personData.Country,
+                "phone": personData.Phone,
+                "address": personData.Address,
+                "CPR": personData.CprNumber,
+                "NemID": body
+            }
+            var serializedObject = JSON.stringify(jsonObject);
+
+            var writeStream = fs.createWriteStream(`${jsonObject.CPR}.msgpack`);
+            var encodeStream = msgpack.createEncodeStream();
+            encodeStream.pipe(writeStream);
+            encodeStream.write(serializedObject);
+            encodeStream.end();
+
+        });
 }
